e233034d80e66c130632adfcf8ef2a3858553f55
Yubao Zhao <zhaoyubao1981@gmail.com>
2016-11-17 13:36:49 -0800

    Adding MEAN Store

---
+++ server/models/customer.js
@@ -0,0 +1,8 @@
+console.log('Customer model is loaded');
+
+const db = require('mongoose'),
+    CustomerSchema = new db.Schema({
+        name: {type: String, unique: true, required:true}
+    }, {timestamps: true});
+
+module.exports = db.model('Customer', CustomerSchema);
